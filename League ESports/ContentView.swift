e40c7c7a9bd52f8113480b0c579f81720d91adc1
Enzo Han <pridealot@gmail.com>
2020-12-30 17:31:46 -0800

    Initial Commit

---
+++ League ESports/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  League ESports
+//
+//  Created by Enzo Han on 12/30/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ League ESports/League_ESportsApp.swift
@@ -0,0 +1,17 @@
+//
+//  League_ESportsApp.swift
+//  League ESports
+//
+//  Created by Enzo Han on 12/30/20.
+//
+
+import SwiftUI
+
+@main
+struct League_ESportsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
